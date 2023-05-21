d68cd4ee878df8e0ee3fc958c21c9d64db281033
Ernest <inyange8888@gmail.com>
2023-05-21 09:43:04 +0100

    fixes

--- src/main/java/com/datastructureandalgorithm/all/arrays/OrderedArrayImpl.java
+++ src/main/java/com/datastructureandalgorithm/all/arrays/OrderedArrayImpl.java
@@ -6,8 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderedArrayImpl {
     public static void main(String[] args) {
         int[] elements = {10, 20, 30, 40, 50, 60, 70};
-        OrderedArray array = new OrderedArray(elements);
-        String result = array.find(30);
-        log.info("RESULT -> {}", result);
+//        OrderedArray array = new OrderedArray(elements);
+//        String result = array.find(30);
+//        log.info("RESULT -> {}", result);
     }
 }
