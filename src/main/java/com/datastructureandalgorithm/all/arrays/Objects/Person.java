2324d1a812d11d78f91282961940e64f75195bc5
Ernest <inyange8888@gmail.com>
2023-05-22 10:29:35 +0100

    fixes

--- src/main/java/com/datastructureandalgorithm/all/arrays/Objects/Person.java
+++ src/main/java/com/datastructureandalgorithm/all/arrays/Objects/Person.java
@@ -1,24 +1,79 @@
 package com.datastructureandalgorithm.all.arrays.Objects;
 
+import lombok.Data;
+
+@Data
 public class Person {
 
-        private String lastName;
-        private String firstName;
-        private int age;
+    private String lastName;
+    private String firstName;
+    private int age;
 
-        public Person(String last, String first, int age) {
-            lastName = last;
-            firstName = first;
-            this.age = age;
+    public Person(String lastName, String firstName, int age) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public void displayPerson() {
+        System.out.print(" Last name: " + lastName);
+        System.out.print(", First name: " + firstName);
+        System.out.println(", Age: " + age);
+    }
+}
+
+class ClassDataArray {
+    private Person[] ArrayOfPersons; // reference to array
+    private int numberOfElements; // number of data items
+
+    // constructor
+    public ClassDataArray(int max) {
+        ArrayOfPersons = new Person[max]; // create the array
+        numberOfElements = 0; // no items yet
+    }
+
+    // find specified value
+    public Person find(String searchName) {
+        int j;
+        for (j = 0; j < numberOfElements; j++) // for each element,
+            if (ArrayOfPersons[j].getLastName().equals(searchName)) // found item?
+                break; // exit loop before end
+        if (j == numberOfElements) // gone to end?
+            return null; // yes, can’t find it
+        else
+            return ArrayOfPersons[j]; // no, found it
+    }
+
+    // put person into array
+    public void insert(String last, String first, int age) {
+        ArrayOfPersons[numberOfElements] = new Person(last, first, age);
+        numberOfElements++; // increment size
+    }
+
+
+    // delete person from array
+    public boolean delete(String searchName) {
+        int j;
+        for (j = 0; j < numberOfElements; j++) { // look for it
+            if (ArrayOfPersons[j].getLastName().equals(searchName))
+                break;
         }
-
-        public void displayPerson()
+        if (j == numberOfElements) {
+            return false;
+        } else // found it
         {
-            System.out.print(" Last name: " + lastName);
-            System.out.print(", First name: " + firstName);
-            System.out.println(", Age: " + age);
+            for (int k = j; k < numberOfElements; k++) // shift down
+                ArrayOfPersons[k] = ArrayOfPersons[k + 1];
+            numberOfElements--; // decrement size
+            return true;
         }
-        public String getLast() // get last name
-        { return lastName; }
+    }
+
+    public void displayArrayOfPersons() // displays array contents
+    {
+        for (int j = 0; j < numberOfElements; j++) // for each element,
+            ArrayOfPersons[j].displayPerson(); // display it
+    }
+
 
 }
