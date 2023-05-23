5b438a90f79cf142ad7395a1d4063c660b6a2003
Ernest <inyange8888@gmail.com>
2023-05-23 15:25:19 +0100

    SORTING- BUBBLE SORT ALGORITHM

---
+++ src/main/java/com/datastructureandalgorithm/all/sorting/simpleSort/ArrayBub.java
@@ -0,0 +1,39 @@
+package com.datastructureandalgorithm.all.sorting.simpleSort;
+
+public class ArrayBub {
+
+    private long[] a; // ref to array a
+    private int nElems; // number of data items
+
+    public ArrayBub(int max) {
+        a = new long[max]; // create the array
+        nElems = 0; // no items yet
+    }
+
+    // put element into array
+    public void insert(long value) {
+        a[nElems] = value; // insert it
+        nElems++; // increment size
+    }
+
+    // displays array contents
+    public void display() {
+        for (int j = 0; j < nElems; j++) // for each element,
+            System.out.print(a[j] + " "); // display it
+        System.out.println("");
+    }
+
+    public void bubbleSort() {
+        int out, in;
+        for (out = nElems - 1; out > 1; out--) // outer loop (backward)
+            for (in = 0; in < out; in++) // inner loop (forward)
+                if (a[in] > a[in + 1]) // out of order?
+                    swap(in, in + 1); // swap them
+    }
+
+    private void swap(int one, int two) {
+        long temp = a[one];
+        a[one] = a[two];
+        a[two] = temp;
+    }
+}
