cb0b2eee8fe4929ce959ed649d29b1199a4d5dc7
Ernest <inyange8888@gmail.com>
2023-05-28 03:22:37 +0100

    SORTING- INSERTION SORT ALGORITHM

--- src/main/java/com/datastructureandalgorithm/all/sorting/simpleSort/insertionSort/InsertionSort.java
+++ src/main/java/com/datastructureandalgorithm/all/sorting/simpleSort/insertionSort/InsertionSort.java
@@ -1,6 +1,44 @@
 package com.datastructureandalgorithm.all.sorting.simpleSort.insertionSort;
 
 public class InsertionSort {
+
+    private long[] a;
+    private int nElems; // number of data items
+
+    //constructor
+    public InsertionSort(int max)     {
+        a = new long[max]; // create the array
+        nElems = 0; // no items yet
+    }
+
+    // put element into array
+    public void insert(long value)     {
+        a[nElems] = value; // insert it
+        nElems++; // increment size
+    }
+
+    public void display() // displays array contents
+    {
+        for(int j=0; j<nElems; j++) // for each element,
+            System.out.print(a[j] + " "); // display it
+        System.out.println("");
+    }
+
+    public void insertionSort()    {
     int in, out;
+
+        for(out=1; out<nElems; out++) // out is dividing line
+        {
+            long temp = a[out]; // remove marked item
+            in = out; // start shifts at out
+            while(in>0 && a[in-1] >= temp) // until one is smaller,
+            {
+                a[in] = a[in-1]; // shift item right,
+                --in; // go left one position
+            }
+            a[in] = temp; // insert marked item
+        } // end for
+
+    }
 
 }
