f98ef056dce092e817caf98f7cdf3b2056ef0585
Ernest <inyange8888@gmail.com>
2023-05-29 05:01:38 +0100

    SORTING- INSERTION SORT ALGORITHM: OBJECTS

--- src/main/java/com/datastructureandalgorithm/all/sorting/simpleSort/insertionSort/primitives/InsertionSortImpl.java
+++ src/main/java/com/datastructureandalgorithm/all/sorting/simpleSort/insertionSort/primitives/InsertionSortImpl.java
@@ -7,17 +7,23 @@ public class InsertionSortImpl {
         int maxSize = 100; // array size
         InsertionSort arr = new InsertionSort(maxSize); // create the array
 
-        // insert 10 items
-        arr.insert(77);
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(00);
-        arr.insert(66);
-        arr.insert(33);
+        // fill array with random numbers
+        for (int j = 0; j < maxSize; j++)         {
+            long n = (long) (java.lang.Math.random() * (maxSize - 1));
+            arr.insert(n);
+        }
+
+//        // insert 10 items
+//        arr.insert(77);
+//        arr.insert(99);
+//        arr.insert(44);
+//        arr.insert(55);
+//        arr.insert(22);
+//        arr.insert(88);
+//        arr.insert(11);
+//        arr.insert(00);
+//        arr.insert(66);
+//        arr.insert(33);
 
         arr.display(); // display items
         arr.insertionSort(); // insertion-sort them
