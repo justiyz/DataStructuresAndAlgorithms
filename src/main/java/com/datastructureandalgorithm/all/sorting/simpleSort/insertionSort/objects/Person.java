a3becb19bdc2cf4f4dd3f1e55df793fff38e4308
Ernest <inyange8888@gmail.com>
2023-05-29 04:03:59 +0100

    SORTING- INSERTION SORT ALGORITHM: OBJECTS

--- src/main/java/com/datastructureandalgorithm/all/sorting/simpleSort/insertionSort/objects/Person.java
+++ src/main/java/com/datastructureandalgorithm/all/sorting/simpleSort/insertionSort/objects/Person.java
@@ -17,7 +17,7 @@ class Person {
         System.out.println(", Age: " + age);
     }
 
-    public String getLast() // get last name
+    public String getLastName() // get last name
     {
         return lastName;
     }
@@ -26,40 +26,40 @@ class Person {
 
 class ArrayInOb {
 
-    private Person[] a; // ref to array a
-    private int nElems; // number of data items
+    private final Person[] array; // ref to array a
+    private int numberOfElements; // number of data items
 
     public ArrayInOb(int max) {
-        a = new Person[max]; // create the array
-        nElems = 0; // no items yet
+        array = new Person[max]; // create the array
+        numberOfElements = 0; // no items yet
     }
 
     // put person into array
     public void insert(String last, String first, int age) {
-        a[nElems] = new Person(last, first, age);
-        nElems++; // increment size
+        array[numberOfElements] = new Person(last, first, age);
+        numberOfElements++; // increment size
     }
 
     // displays array contents
     public void display() {
-        for (int j = 0; j < nElems; j++) // for each element,
-            a[j].displayPerson(); // display it
+        for (int j = 0; j < numberOfElements; j++) // for each element,
+            array[j].displayPerson(); // display it
         System.out.println("");
     }
 
     //sort the person data by the last name
     public void insertionSort() {
         int in, out;
-        for (out = 1; out < nElems; out++) // out is dividing line
+        for (out = 1; out < numberOfElements; out++) // out is dividing line
         {
-            Person temp = a[out]; // remove marked person
+            Person temp = array[out]; // remove marked person
             in = out; // start shifting at out
             while (in > 0 && // until smaller one found,
-                    a[in - 1].getLast().compareTo(temp.getLast()) > 0) {
-                a[in] = a[in - 1]; // shift item to the right
+                    array[in - 1].getLastName().compareTo(temp.getLastName()) > 0) {
+                array[in] = array[in - 1]; // shift item to the right
                 --in; // go left one position
             }
-            a[in] = temp; // insert marked item
+            array[in] = temp; // insert marked item
         } // end for
     } // end insertionSort()
 }
