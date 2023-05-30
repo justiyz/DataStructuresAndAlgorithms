8aefea73b8d45d09b4215cdbda359853590db86f
Ernest <inyange8888@gmail.com>
2023-05-30 06:03:31 +0100

    STACKS & QUEUES- STACKS

--- src/main/java/com/datastructureandalgorithm/all/stacks/Stacks.java
+++ src/main/java/com/datastructureandalgorithm/all/stacks/Stacks.java
@@ -1,4 +1,20 @@
 package com.datastructureandalgorithm.all.stacks;
 
 public class Stacks {
+
+    private int maxSize; // size of stack array
+    private long[] stackArray;
+    private int top; // top of stack
+
+    // constructor
+    public Stacks(int s)     {
+        maxSize = s; // set array size
+        stackArray = new long[maxSize]; // create array
+        top = -1; // no items yet
+    }
+
+    public void push(long j) // put item on top of stack
+    {
+        stackArray[++top] = j; // increment top, insert item
+    }
 }
