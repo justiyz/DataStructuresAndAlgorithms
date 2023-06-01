8b66c510832054eebf1cc95b8eb0a54df5c126ae
Ernest <inyange8888@gmail.com>
2023-06-01 16:23:44 +0100

    STACKS & QUEUES- STACKS

---
+++ src/main/java/com/datastructureandalgorithm/all/stacks/objects/ReverserImpl.java
@@ -0,0 +1,34 @@
+package com.datastructureandalgorithm.all.stacks.objects;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverserImpl {
+    public static void main(String[] args) throws IOException {
+
+        String input, output;
+        while(true)
+        {
+            System.out.print("Enter a string: ");
+            System.out.flush();
+            input = getString(); // read a string from kbd
+            if( input.equals("") ) // quit if [Enter]
+                break;
+
+            // make a Reverser
+            Reverser theReverser = new Reverser(input);
+            output = theReverser.doRev(); // use it
+            System.out.println("Reversed: " + output);
+        } // end while
+
+    }
+
+    public static String getString() throws IOException
+    {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+}
