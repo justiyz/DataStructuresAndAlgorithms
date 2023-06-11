1511f8d71e985c83e8d780f07d96368766dd44b9
Ernest <inyange8888@gmail.com>
2023-06-11 15:27:21 +0100

    LINKED LISTS - SIMPLE LINKED LIST

--- src/main/java/com/datastructureandalgorithm/all/linkedList/simpleLinkedList/LinkList.java
+++ src/main/java/com/datastructureandalgorithm/all/linkedList/simpleLinkedList/LinkList.java
@@ -13,5 +13,11 @@ class LinkList {
         return (first == null);
     }
 
+    // insert at start of list
+    public void insertFirst(int id, double dd) {
+        Link newLink = new Link(id, dd); // make new link
+        newLink.next = first; // newLink --> old first
+        first = newLink; // first --> newLink
+    }
 
 }
