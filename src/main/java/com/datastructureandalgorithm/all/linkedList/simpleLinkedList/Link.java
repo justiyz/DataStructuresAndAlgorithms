1ad7f3cc590dd1770351db12d57c285cc3a464bb
Ernest <inyange8888@gmail.com>
2023-06-11 07:29:09 +0100

    LINKED LISTS - SIMPLE LINKED LIST

--- src/main/java/com/datastructureandalgorithm/all/linkedList/simpleLinkedList/Link.java
+++ src/main/java/com/datastructureandalgorithm/all/linkedList/simpleLinkedList/Link.java
@@ -1,21 +1,18 @@
 package com.datastructureandalgorithm.all.linkedList.simpleLinkedList;
 
-class Link
-{
+class Link {
     public int iData; // data item
     public double dData; // data item
     public Link next; // next link in list
 
     // constructor
-    public Link(int id, double dd)
-    {
+    public Link(int id, double dd) {
         iData = id; // initialize data
         dData = dd; // (‘next’ is automatically
     } // set to null)
 
     // display ourself
-    public void displayLink()
-    {
+    public void displayLink() {
         System.out.print("{" + iData + ", " + dData + "} ");
     }
 } // end class Link
