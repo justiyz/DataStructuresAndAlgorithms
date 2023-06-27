c8d9f59816224917f8d10c62866174141235a96b
Ernest <inyange8888@gmail.com>
2023-06-27 22:21:25 +0100

    LINKED LISTS - DOUBLY LINKED LISTS

--- src/main/java/com/datastructureandalgorithm/all/linkedList/moreExamples/listInsertionSort/ListInsertionSortImpl.java
+++ src/main/java/com/datastructureandalgorithm/all/linkedList/moreExamples/listInsertionSort/ListInsertionSortImpl.java
@@ -7,27 +7,27 @@ class ListInsertionSortImpl {
         // create array of links
         Link[] linkArray = new Link[size];
 
-        for(int j=0; j<size; j++) // fill array with links random number
+        for (int j = 0; j < size; j++) // fill array with links random number
         {
-            int n = (int)(java.lang.Math.random()*99);
+            int n = (int) (java.lang.Math.random() * 99);
             Link newLink = new Link(n); // make link
             linkArray[j] = newLink; // put in array
         }
 
         // display array contents
         System.out.print("Unsorted array: ");
-        for(int j=0; j<size; j++)
-            System.out.print( linkArray[j].dData + " " );
+        for (int j = 0; j < size; j++)
+            System.out.print(linkArray[j].dData + " ");
         System.out.println("");
 
         // create new list initialized with array
         SortedList theSortedList = new SortedList(linkArray);
-        for(int j=0; j<size; j++) // links from list to array
+        for (int j = 0; j < size; j++) // links from list to array
             linkArray[j] = theSortedList.remove();
 
         // display array contents
         System.out.print("Sorted Array: ");
-        for(int j=0; j<size; j++)
+        for (int j = 0; j < size; j++)
             System.out.print(linkArray[j].dData + " ");
         System.out.println("");
     }
