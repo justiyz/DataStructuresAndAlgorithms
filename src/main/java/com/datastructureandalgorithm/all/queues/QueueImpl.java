0d8ee204d374e00f1b3ff1ed733de7ac81daa6e1
Ernest <inyange8888@gmail.com>
2023-07-07 20:49:09 +0100

    fixes

--- src/main/java/com/datastructureandalgorithm/all/queues/QueueImpl.java
+++ src/main/java/com/datastructureandalgorithm/all/queues/QueueImpl.java
@@ -1,23 +1,31 @@
 package com.datastructureandalgorithm.all.queues;
 
-public class QueueImpl {
+class QueueImpl {
     public static void main(String[] args) {
 
-        Queue theQueue = new Queue(5); // queue holds 5 items
-        theQueue.insert(10); // insert 4 items
+        // queue holds 5 items
+        Queue theQueue = new Queue(5);
+
+        // insert 4 items
+        theQueue.insert(10);
         theQueue.insert(20);
         theQueue.insert(30);
         theQueue.insert(40);
-        theQueue.remove(); // remove 3 items
-        theQueue.remove(); // (10, 20, 30)
+
+        // remove 3 items (10, 20, 30)
         theQueue.remove();
-        theQueue.insert(50); // insert 4 more items
-        theQueue.insert(60); // (wraps around)
+        theQueue.remove();
+        theQueue.remove();
+
+        // insert 4 more items (wraps around)
+        theQueue.insert(50);
+        theQueue.insert(60);
         theQueue.insert(70);
         theQueue.insert(80);
 
-        while( !theQueue.isEmpty() ) // remove and display
-        { long n = theQueue.remove(); // (40, 50, 60, 70, 80)
+        // remove and display (40, 50, 60, 70, 80)
+        while( !theQueue.isEmpty() )
+        { long n = theQueue.remove();
             System.out.print(n);
             System.out.print(" ");
         }
